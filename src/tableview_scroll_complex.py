36315cd6dd6bc34008dea42fb22dacb231248a06
skl.progs <skl.progs@gmail.com>
2022-10-08 02:04:17 +0300

    tableview_scroll_complex: use index logic

--- src/tableview_scroll_complex.py
+++ src/tableview_scroll_complex.py
@@ -2,13 +2,15 @@ import sys
 import PyQt5
 import PyQt5.QtWidgets
 
+from skl_shared_qt.localize import _
+import skl_shared_qt.shared as sh
+
 
 class TableDelegate(PyQt5.QtWidgets.QStyledItemDelegate):
     # akej74, https://stackoverflow.com/questions/35397943/how-to-make-a-fast-qtableview-with-html-formatted-and-clickable-cells
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.rowno = 0
-        self.colno = 0
+        self.match_index = 0
     
     def paint(self,painter,option,index):
         options = PyQt5.QtWidgets.QStyleOptionViewItem(option)
@@ -16,7 +18,7 @@ class TableDelegate(PyQt5.QtWidgets.QStyledItemDelegate):
         style = options.widget.style()
         style.drawControl(PyQt5.QtWidgets.QStyle.CE_ItemViewItem,options,painter)
         
-        if index.row() == self.rowno and index.column() == self.colno:
+        if index == self.match_index:
             color = PyQt5.QtGui.QColor('red')
             pen = PyQt5.QtGui.QPen(color,2)
             painter.setPen(pen)
@@ -46,8 +48,7 @@ class TableModel(PyQt5.QtCore.QAbstractTableModel):
     def columnCount(self,index):
         return len(self._data[0])
     
-    def update(self,rowno,colno):
-        index_ = self.index(rowno,colno)
+    def update(self,index_):
         self.dataChanged.emit(index_,index_)
 
 
@@ -58,71 +59,40 @@ class Table(PyQt5.QtWidgets.QTableView):
         super().__init__()
         self.rownum = 200
         self.colnum = 10
-        self.y = []
         self.delegate = TableDelegate()
         self.setItemDelegate(self.delegate)
-        self.vscroll = self.verticalScrollBar()
         self.set_row_height()
+    
+    def go_down(self):
+        f = '[QtTests] tableview_scroll_complex.Table.go_down'
+        old_index = self.currentIndex()
+        rowno = old_index.row()
+        colno = old_index.column()
+        #TODO: elaborate
+        rowno += 1
+        new_index = self.mymodel.index(rowno,colno)
+        self.setCurrentIndex(new_index)
+        self.delegate.match_index = new_index
+        self.mymodel.update(old_index)
+        self.mymodel.update(new_index)
+    
+    def keyPressEvent(self,event):
+        #if event.key() == PyQt5.QtCore.Qt.Key_Down:
+        if event.key() == PyQt5.QtCore.Qt.Key_Q:
+            print('Going down...')
+            self.go_down()
+            #self.scrollTo(self.index,PyQt5.QtWidgets.QAbstractItemView.PositionAtTop)
+            #self.print_cell()
+        super().keyPressEvent(event)
     
     def set_row_height(self,height=42):
         for no in range(self.rownum):
             self.setRowHeight(no,height)
     
-    def select(self,rowno,colno):
-        if rowno == self.delegate.rowno and colno == self.delegate.colno:
-            return
-        self.mymodel.update(self.delegate.rowno,self.delegate.colno)
-        self.mymodel.update(rowno,colno)
-        self.delegate.rowno = rowno
-        self.delegate.colno = colno
-    
-    def set_all_y(self):
-        for rowno in range(self.rownum):
-            self.y.append(self.rowViewportPosition(rowno))
-        print(self.y)
-    
-    def get_page_y(self,y):
-        height = self.height()
-        print('Table height:',height)
-        y += 42
-        page_no = int(y/height)
-        print('Page #{}'.format(page_no))
-        y = page_no * height
-        #y = page_no * height + (page_no + 1) * 42
-        mes = 'Page Y: {}'.format(y)
-        print(mes)
-        return y
-    
-    def get_scroll(self,y):
-        max_ = self.vscroll.maximum()
-        #TODO: do not hardcode row height
-        #y += 42
-        y = self.get_page_y(y)
-        scrolly = int((max_*y)/(self.y[-1]))
-        mes = 'Scrolling percentage: {}'.format(scrolly)
-        print(mes)
-        return scrolly
-    
     def print_cell(self):
-        mes = '"' + self.matrix[self.delegate.rowno][self.delegate.colno] + '"'
-        print(mes)
-    
-    def set_scroll(self,y):
-        self.vscroll.setValue(0)
-        percent = self.get_scroll(y)
-        self.vscroll.setValue(percent)
-        self.print_cell()
-    
-    def show_row(self,rowno):
-        y = self.y[rowno]
-        self.set_scroll(y)
-    
-    def go_down(self):
-        rowno = self.delegate.rowno
-        colno = self.delegate.colno
-        rowno += 1
-        self.select(rowno,colno)
-        self.show_row(rowno)
+        index_ = self.currentIndex()
+        mes = '"' + str(self.mymodel.data(index_)) + '"'
+        sh.objs.get_mes(f,mes,True).show_debug()
 
 
 
@@ -133,7 +103,7 @@ class App(PyQt5.QtWidgets.QMainWindow):
         self.table = Table()
         self.setCentralWidget(self.table)
         self.setGeometry(300,200,800,600)
-        PyQt5.QtWidgets.QShortcut(PyQt5.QtGui.QKeySequence('Down'),self).activated.connect(self.table.go_down)
+        #PyQt5.QtWidgets.QShortcut(PyQt5.QtGui.QKeySequence('Down'),self).activated.connect(self.table.go_down)
     
     def fill(self):
         self.table.matrix = []
@@ -151,6 +121,5 @@ if __name__ == '__main__':
     exe = PyQt5.QtWidgets.QApplication(sys.argv)
     app = App()
     app.fill()
-    app.table.set_all_y()
     app.show()
     exe.exec_()
